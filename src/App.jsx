c958011445b7d250aa9a3ed1702fc2bc8baad800
Talo Vargas <govargas@gmail.com>
2025-05-13 14:02:46 +0200

    final result of our workshop today

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 // TODO: Import necessary modules and data
+import { BrowserRouter, Routes, Route } from "react-router"
+import Home from './pages/Home'
+import SongInfo from './pages/SongInfo'
 
 export const App = () => {
   return (
     <>
       <h1>Start here in app.jsx</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/songinfo" element={<SongInfo />} />
+    </Routes>
+
+
       {/* TODO: Set up the two routes. One for the list of songs, one for the song details */}
+
+    </BrowserRouter>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,27 @@
 // TODO: Import necessary modules and data
+import { songs } from '../data/songs.json'
+
 
 const Home = () => {
   // TODO: Implement logic to display the list of songs
 
+  
   return (
-
+    <>
     <main>
-      <h1>Rolling Stones 500 Greatest Songs of all Time</h1>
-      {/* TODO: Display the list of songs with links */}
+    <h1>Rolling Stones 500 Greatest Songs of all Time</h1>
+      {/* TODO: Display the list of songs with links 
+        */}
+    <ul>
+      {songs.map((song) => (
+        <li>{song.title}</li>
+      ))}  
+    </ul>
+    <div>{console.log(songs)}</div>
     </main>
+    </>
+
+
 
   );
 }
